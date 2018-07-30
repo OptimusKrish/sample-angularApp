3516ccb898ab21b0d4f3ca9ea6ce7ca3ce46f049
Muralidharan, Krishnakumar <Krishnakumar.Muralidharan@news.co.uk>
2018-07-30 17:41:57 +0530

    To render HTML from node

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ var express = require('express');
 var path = require('path');
 var app = express();
 
-// Public assets
-app.use('/js', express.static('js'));
+// Render HTML
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
+// Public assets
+app.use('/public', express.static('public'));
+
+// Index Route
 app.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname+'/index.html'));
+  res.render('index.html');
 });
 
 app.listen('3000', () => {
